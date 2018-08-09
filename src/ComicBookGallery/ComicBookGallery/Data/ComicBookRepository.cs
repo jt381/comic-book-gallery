b3b443dfd3810b69956dab600b68bee806ef4653
jontoomey <j.toomey@exeter.ac.uk>
2018-08-09 09:15:55 +0100

    Added comic books list view

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -61,6 +61,13 @@ namespace ComicBookGallery.Data
             }
 
         };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
